634035acca608bde7efed22cc95a2260b62b104b
SERGIO SOLER MARÍN <Sergio.Soler@alu.uclm.es>
2022-09-27 09:50:40 +0200

    segundo commint 27 del 9

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-
+const fs = require('fs');
 const express = require('express');
-const uuid = require('uuid/v4');
 const app = express();
 
+//HTTP GET POST PUT DELETE
+/*
+get "/"
+get "/obtenerPartidas"
+post get "/agregarUsuario/:nick"
+put "/actualizarPartida"
+delete "/"
+*/
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send("Hola)
-    .end();
+
+app.get('/', (req,res)=> {
+    res
+        .status(200)
+        .send("Hola")
+        .end();
 });
-// Start the server
+
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
-// [END gae_flex_quickstart]
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl + C to quit.');
+})
