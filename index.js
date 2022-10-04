84d4dd382efd4748acd3f91076f38f4b615a094e
SERGIO SOLER MARÍN <Sergio.Soler@alu.uclm.es>
2022-10-04 10:41:44 +0200

    4 del 10 de 2022

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
+const modelo = require("./servidor/modelo.js");
+
+let juego = new modelo.Juego();
 
 //HTTP GET POST PUT DELETE
 /*
@@ -12,12 +15,53 @@ delete "/"
 */
 
 
-app.get('/', (req,res)=> {
-    res
-        .status(200)
-        .send("Hola")
-        .end();
+//app.get('/', (req,res)=> {
+  //  res
+    ////  .send("Hola")
+        //.end();
+//});
+
+app.use(express.static(__dirname + "/"));
+
+app.get("/", function(request,response){
+	var contenido=fs.readFileSync(__dirname+"/cliente/index.html");
+	response.setHeader("Content-type","text/html");
+	response.send(contenido);
 });
+
+app.get("/agregarUsuario/:nick",function(request,response){
+  let nick = request.params.nick;
+  let res=juego.agregarUsuario(nick);
+  response.send(res);//
+});
+
+app.get("/crearPartida/:nick",function(request,response){
+  let nick = request.params.nick;
+  let res = juego.jugadorCreaPartida(nick);
+  response.send(res);
+});
+
+app.get("/unirseAPartida/:nick/:codigo",function(request,response){
+  let nick = request.params.nick;
+  let codigo = request.params.codigo;
+  let res = juego.jugadorSeUneAPartida(nick,codigo);
+  response.send(res);
+});
+
+app.get("/obtenerPartidas",function(request,response){
+  let lista=juego.obtenerPartidas();
+  response.send(lista);
+});
+
+app.get("/obtenerPartidasDisponibles",function(request,response){
+  let lista=juego.obtenerPartidasDisponibles();
+  response.send(lista);
+});
+
+
+
+
+
 
 const PORT = process.env.PORT || 8080;
 
---
+++ servidor/modelo.js
@@ -0,0 +1,112 @@
+function Juego(){
+	this.partidas={};
+	this.usuarios={}; //array asociativo
+
+	this.agregarUsuario=function(nick){
+		let res={"nick":-1};
+		if (!this.usuarios[nick]){
+			this.usuarios[nick]=new Usuario(nick,this);
+			res={"nick":nick};
+			console.log("Nuevo usuario: "+nick);
+		}
+		return res;
+	}
+	this.eliminarUsuario=function(nick){
+		delete this.usuarios[nick];
+	}
+	this.jugadorCreaPartida=function(nick){
+		let usr = this.usuarios[nick];
+		let res={codigo:-1};
+  		if (usr){
+    		let codigo=usr.crearPartida();
+	    	//let codigo=this.crearPartida(usr);
+	    	res={codigo:codigo};
+	    }
+    	return res;
+	}
+	this.jugadorSeUneAPartida=function(nick,codigo){
+		let usr = this.usuarios[nick];
+		let res={"codigo":-1};
+  		if (usr){
+    		let valor=usr.unirseAPartida(codigo);
+    		//let valor=this.unirseAPartida(codigo,usr)
+	    	res={"codigo":valor};
+	    }
+    	return res;
+	}
+	this.crearPartida=function(usr){
+		let codigo=Date.now();
+		console.log("Usuario "+usr.nick+ " crea partida "+codigo);
+		this.partidas[codigo]=new Partida(codigo,usr); 
+		return codigo;
+	}
+	this.unirseAPartida=function(codigo,usr){
+		let res=-1;
+		if (this.partidas[codigo]){
+			res=this.partidas[codigo].agregarJugador(usr);
+		}
+		else{
+			console.log("La partida no existe");
+		}
+		return res;
+	}
+	this.obtenerPartidas=function(){
+		let lista=[];
+		for (let key in this.partidas){
+			lista.push({"codigo":key,"owner":this.partidas[key].owner});
+		}
+		return lista;
+	}
+	this.obtenerPartidasDisponibles=function(){
+		//devolver sólo las partidas sin completar
+		let lista=[];
+		for (let key in this.partidas){
+			lista.push({"codigo":key,"owner":this.partidas[key].owner.nick});
+		}
+		return lista;
+	}
+}
+
+function Usuario(nick,juego){
+	this.nick=nick;
+	this.juego=juego;
+	this.crearPartida=function(){
+		return this.juego.crearPartida(this)
+	}
+	this.unirseAPartida=function(codigo){
+		return this.juego.unirseAPartida(codigo,this);
+	}
+}
+
+function Partida(codigo,usr){
+	this.codigo=codigo;
+	this.owner=usr;
+	this.jugadores=[];
+	this.fase="inicial"; //new Inicial()
+	this.maxJugadores=2;
+	this.agregarJugador=function(usr){
+		let res=this.codigo;
+		if(this.hayHueco()){
+			this.jugadores.push(usr);
+			console.log("El usuario "+usr.nick+" se une a la partida "+this.codigo);
+			this.comprobarFase();
+		}
+		else{
+			res=-1;
+			console.log("La partida está completa")
+		}
+		return res;
+	}
+	this.hayHueco=function(){
+	
+		return (this.jugadores.length<this.maxJugadores);
+	}
+	this.comprobarFase=function(){
+		if(!this.hayHueco()){
+			this.fase="jugando";
+		}
+	}
+	this.agregarJugador(this.owner);
+}
+
+module.exports.Juego = Juego;
